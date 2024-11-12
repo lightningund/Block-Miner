36eb64e3b76c80a223dd1dbe0e56490d75f7a271
lightningund <lightningundies0826@gmail.com>
2024-11-12 13:16:20 -0600

    added gossip creation

--- chain.cpp
+++ chain.cpp
@@ -4,20 +4,28 @@
 #include <array>
 #include <vector>
 #include <unordered_set>
+#include <chrono>
 #include <boost/asio.hpp>
 
 #define LOG_ERR(msg) std::cerr << (msg) << ": " << errno << "\n"
 
 constexpr auto peers_to_repeat_to = 3;
 
-using string = std::string;
+using std::string;
 using msg_id_t = string;
 using host_t = string;
 using port_t = unsigned int;
 using name_t = string;
 
+using stamp_t = long long;
+
+stamp_t get_timestamp() {
+	using namespace std::chrono;
+	return duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+}
+
 host_t my_host = "127.0.0.1";
-port_t my_port = 9000;
+port_t my_port = 50000;
 name_t my_name = "Ben's Computer";
 
 struct Block {
@@ -53,29 +61,66 @@ void process_gossip(const Gossip& incoming) {
 	// Send incoming to 3 other peers
 }
 
+string make_gossip_json() {
+	string result = "{";
+	result += "\"type\": \"GOSSIP\",";
+	result += "\"host\": \"" + my_host + "\",";
+	result += "\"port\": " + std::to_string(my_port) + ",";
+	result += "\"id\": " + std::to_string(get_timestamp()) + ",";
+	result += "\"name\": \"" + my_name + "\"}";
+	return result;
+}
+
 int main() {
 	using boost::asio::ip::udp;
 
 	boost::asio::io_context io_ctxt{};
 
-	udp::socket socket{io_ctxt, udp::endpoint{udp::v4(), 0}};
+	udp::endpoint us_ep = udp::endpoint{udp::v4(), my_port};
+	udp::socket us_sock{io_ctxt, us_ep};
 
 	std::cout << "Made Socket\n";
+
+	my_host = boost::asio::ip::host_name();
+
+	std::cout << "Our Address: " << my_host << "\n";
+	std::cout << "Our Port: " << my_port << "\n";
+
+	std::cout << "HMM: " << boost::asio::ip::host_name() << "\n";
 
 	udp::resolver resolver{io_ctxt};
 	udp::endpoint silicon = *resolver.resolve({udp::v4(), "silicon.cs.umanitoba.ca", "8999"});
 
-	std::cout << "Made Endpoint\n";
+	std::cout << "Made Silicon Endpoint\n";
 
-	std::string msg = "{\"type\": \"STATS\"}";
-	socket.send_to(boost::asio::buffer(msg), silicon);
+	string msg = "{\"type\": \"STATS\"}";
+	us_sock.send_to(boost::asio::buffer(msg), silicon);
 
 	std::cout << "Sent message\n";
 
 	while (true) {
 		std::array<char, 1024> buf;
 		boost::system::error_code error;
-		size_t len = socket.receive(boost::asio::buffer(buf));
+		size_t len = us_sock.receive(boost::asio::buffer(buf));
+
+		std::cout.write(buf.data(), len);
+		std::cout << "\n" << len << "\n";
+
+		if (len < buf.size()) {
+			break; // All out of data
+		}
+	}
+
+	string goss = make_gossip_json();
+	std::cout << goss << "\n";
+	us_sock.send_to(boost::asio::buffer(goss), silicon);
+
+	std::cout << "Sent Gossip\n";
+
+	while (true) {
+		std::array<char, 1024> buf;
+		boost::system::error_code error;
+		size_t len = us_sock.receive(boost::asio::buffer(buf));
 
 		std::cout.write(buf.data(), len);
 		std::cout << "\n" << len << "\n";
