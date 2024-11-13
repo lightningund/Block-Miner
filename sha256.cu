f3e2b1ceda24305f8d309d2ee862cd06a4d116d8
lightningund <lightningundies0826@gmail.com>
2024-11-13 14:57:17 -0600

    more formatting

--- sha256.cu
+++ sha256.cu
@@ -44,26 +44,26 @@ typedef struct {
 
 /**************************** VARIABLES *****************************/
 __constant__ WORD k[64] = {
-	0x428a2f98,0x71374491,0xb5c0fbcf,0xe9b5dba5,0x3956c25b,0x59f111f1,0x923f82a4,0xab1c5ed5,
-	0xd807aa98,0x12835b01,0x243185be,0x550c7dc3,0x72be5d74,0x80deb1fe,0x9bdc06a7,0xc19bf174,
-	0xe49b69c1,0xefbe4786,0x0fc19dc6,0x240ca1cc,0x2de92c6f,0x4a7484aa,0x5cb0a9dc,0x76f988da,
-	0x983e5152,0xa831c66d,0xb00327c8,0xbf597fc7,0xc6e00bf3,0xd5a79147,0x06ca6351,0x14292967,
-	0x27b70a85,0x2e1b2138,0x4d2c6dfc,0x53380d13,0x650a7354,0x766a0abb,0x81c2c92e,0x92722c85,
-	0xa2bfe8a1,0xa81a664b,0xc24b8b70,0xc76c51a3,0xd192e819,0xd6990624,0xf40e3585,0x106aa070,
-	0x19a4c116,0x1e376c08,0x2748774c,0x34b0bcb5,0x391c0cb3,0x4ed8aa4a,0x5b9cca4f,0x682e6ff3,
-	0x748f82ee,0x78a5636f,0x84c87814,0x8cc70208,0x90befffa,0xa4506ceb,0xbef9a3f7,0xc67178f2
+	0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+	0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+	0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+	0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+	0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+	0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+	0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+	0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
 };
 
 /*********************** FUNCTION DEFINITIONS ***********************/
 __device__
 __forceinline__
 void cuda_sha256_transform(CUDA_SHA256_CTX *ctx, const BYTE data[]) {
-	WORD a, b, c, d, e, f, g, h, i, j, t1, t2, m[64];
+	WORD a, b, c, d, e, f, g, h, t1, t2, m[64];
 
-	for (i = 0, j = 0; i < 16; ++i, j += 4) {
-		m[i] = (data[j] << 24) | (data[j + 1] << 16) | (data[j + 2] << 8) | (data[j + 3]);
+	for (int j = 0; j < 64; j += 4) {
+		m[j / 4] = (data[j] << 24) | (data[j + 1] << 16) | (data[j + 2] << 8) | (data[j + 3]);
 	}
-	for ( ; i < 64; ++i) {
+	for (int i = 16; i < 64; ++i) {
 		m[i] = SIG1(m[i - 2]) + m[i - 7] + SIG0(m[i - 15]) + m[i - 16];
 	}
 
@@ -76,9 +76,9 @@ void cuda_sha256_transform(CUDA_SHA256_CTX *ctx, const BYTE data[]) {
 	g = ctx->state[6];
 	h = ctx->state[7];
 
-	for (i = 0; i < 64; ++i) {
-		t1 = h + EP1(e) + CH(e,f,g) + k[i] + m[i];
-		t2 = EP0(a) + MAJ(a,b,c);
+	for (int i = 0; i < 64; ++i) {
+		t1 = h + EP1(e) + CH(e, f, g) + k[i] + m[i];
+		t2 = EP0(a) + MAJ(a, b, c);
 		h = g;
 		g = f;
 		f = e;
@@ -115,9 +115,7 @@ void cuda_sha256_init(CUDA_SHA256_CTX *ctx) {
 
 __device__
 void cuda_sha256_update(CUDA_SHA256_CTX *ctx, const BYTE data[], size_t len) {
-	WORD i;
-
-	for (i = 0; i < len; ++i) {
+	for (size_t i = 0; i < len; ++i) {
 		ctx->data[ctx->datalen] = data[i];
 		ctx->datalen++;
 		if (ctx->datalen == 64) {
@@ -130,9 +128,7 @@ void cuda_sha256_update(CUDA_SHA256_CTX *ctx, const BYTE data[], size_t len) {
 
 __device__
 void cuda_sha256_final(CUDA_SHA256_CTX *ctx, BYTE hash[]) {
-	WORD i;
-
-	i = ctx->datalen;
+	WORD i = ctx->datalen;
 
 	// Pad whatever data is left in the buffer.
 	if (ctx->datalen < 56) {
